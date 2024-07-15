b201d54760b64ac37a70a3514989a3997482c8f7
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-07-15 20:01:31 +0200

    first commit

---
+++ Chapter 1/ch1_alert.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//alert("saying hi from a different file");
+
+// let myStatus = "new";
+// let scared = true;
+// if (myStatus === "new") {
+//   console.log("Welcome to JavaScript");
+//   if (scared) {
+//     console.log("Don't worry you'll be fine!");
+//   } else {
+//     console.log("You're brave! You are going to do great!");
+//   }
+// } else {
+//   console.log("Welcome back, I knew you'd like it!");
+// }
+
+//let a = 10; //value set to 10
+//console.log(a); //this will print the value of a, which is 10
+
+// let feeling = prompt("Hi Alexander, how are you feeling today?");
+// console.log(feeling);
+
+console.log(Math.floor(Math.random()));
+console.log(Math.floor(Math.random() * 100));
---
+++ readme.txt
@@ -0,0 +1,7 @@
+echo "# Advanced-JavaScript" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AlexanderBadenhorst/Advanced-JavaScript.git
+git push -u origin main
