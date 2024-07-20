ce793fe053908864a0fb9ce94af3d821bf724a8b
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-07-20 11:59:25 +0200

    First commit

---
+++ Chapter 7/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/*****Practice exercise 7.1*****/
+/*class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+let person1 = new Person("Maaike", "van Putten");
+let person2 = new Person("Laurence", "Svekis");
+console.log("hello " + person1.firstname);
+console.log("hello " + person2.firstname);*/
+
+/*****Practice exercise 7.2*****/
+/*class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  fullname() {
+    return this.firstname + " " + this.lastname;
+  }
+}
+let person1 = new Person("Maaike", "van Putten");
+let person2 = new Person("Laurence", "Svekis");
+console.log(person1.fullname());
+console.log(person2.fullname());*/
+
+/*****Practice exercise 7.3*****/
+/*class Animal {
+  constructor(species, sounds) {
+    this.species = species;
+    this.sounds = sounds;
+  }
+  speak() {
+    console.log(this.species + " " + this.sounds);
+  }
+}
+Animal.prototype.eat = function () {
+  return this.species + " is eating";
+};
+let cat = new Animal("cat", "meow");
+let dog = new Animal("dog", "bark");
+cat.speak();
+console.log(dog.eat());
+console.log(dog);*/
